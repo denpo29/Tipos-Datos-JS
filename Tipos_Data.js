4a24908b07e19d674e107d139598de7e6c4b67d1
denpo29 <gonza.dp29@hotmail.com>
2023-02-18 23:44:16 -0300

    Añadiendo el archivo del segundo ejercicio del curso de JS

---
+++ Tipos_Data.js
@@ -0,0 +1,12 @@
+let nombre = "Denis Gonzalo Ponce";
+let edad = 34;
+let soyDesarrollador = false;
+const fecha_nacimiento = new Date("july 29 1988");
+const libroFavorito = {
+    titulo: "Los Amores de Cristina",
+    autor: "Franco Linder",
+    fecha: "marzo de 2013",
+    sitio: "https://www.abebooks.com/9789504931591/amores-Cristina-LINDER-9504931596/plp"
+}
+
+console.log(nombre, edad, soyDesarrollador,fecha_nacimiento,libroFavorito)
